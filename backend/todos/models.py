41b91d5ddc8b44e629aea759bf8b4de80d3ef82d
FredoDiaz <alfredo.avinadiaz@gmail.com>
2019-12-02 12:25:06 -0800

    Added todo models

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=20)
+    desc = models.TextField()
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title}"
